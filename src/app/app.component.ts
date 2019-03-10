4ad6108822231d6f173f8935e2596f2970d29281
mpascualach <miguelpacheson@hotmail.es>
2019-03-10 19:07:50 +0100

    injecting ngFor loops in pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+  tabs: Array<object> = [
+    { text: 'Home', url: '/home' },
+    { text: 'Our projects', url: '/projects' },
+    { text: 'Other works', url: '/other-works' },
+    { text: 'About us', url: '/about' },
+  ]
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Project {
+  id: number,
+  image: string,
+  title: string,
+  description: string
+}
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
+
+
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+
+  projects: Array<object> = [
+    { id: 1, image: '', title: 'Momochi', description: '' },
+    { id: 2, image: '', title: 'Flymmics', description: '' },
+    { id: 3, image: '', title: 'Whoops', description: '' },
+    { id: 4, image: '', title: 'Dead-pet', description: '' },
+  ];
+
+  rows: Array<any> = [];
+
+  constructor() { 
+    for (let i = 0; i < this.projects.length; i++) {
+      
+    }
+  }
 
   ngOnInit() {
+
   }
 
 }
